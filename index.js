64f8857584f37c88ca3821163e6b5cfec72e7d89
JeffHauckJr <jefferyhauckjr@gmail.com>
2021-08-26 13:28:55 -0500

    further work done on responding

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-require('dotenv').config();
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+require("dotenv").config();
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 const TOKEN = process.env.TOKEN;
 
+client.on("ready", () => {
+  console.info(`Logged in as ${client.user.tag}!`);
+});
 
-
-client.on('ready', () => {
-    console.info(`Logged in as ${client.user.tag}!`);
-  });
-
+client.on("messageCreate", (msg) => {
+  if (msg.content === "ping") {
+    msg.channel.send("pong");
+  }
+  if (msg.content === "hello") {
+    msg.channel.send("Hello, I am Inspo");
+  } else if (msg.content.startsWith("!kick")) {
+    if (msg.mentions.users.size) {
+      const taggedUser = msg.mentions.users.first();
+      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    } else {
+      msg.reply("Please tag a valid user!");
+    }
+  }
+});
 
 client.login(TOKEN);
